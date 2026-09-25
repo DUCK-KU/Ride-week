73ee29ca472cc7a46efe585e1251fd7ec5eac003
DUCK-KU <kimduckgeun@gmail.com>
2026-09-25 14:35:07 +0900

    Add private coach training data export

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = 'ride-week-v40';
+const cacheName = 'ride-week-v41';
 const files = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(files)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(names => Promise.all(names.filter(name => name.startsWith('ride-week-') && name !== cacheName).map(name => caches.delete(name)))).then(() => self.clients.claim())));
